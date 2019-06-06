221c0e04dae14f2290bc48683707f6c204910cd9
ZvonimirSun <hi@iszy.xyz>
2019-06-07 02:23:35 +0800

    update

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.listen(port, () => {
 bot.onText(/\/start/, msg => {
   if (msg.chat.id == admin) {
     bot.sendMessage(msg.chat.id, "Welcome!", {
-      parse_mode: "Markdown",
       reply_markup: {
         keyboard: [["asf", "hentai"]]
       }
@@ -46,7 +45,6 @@ bot.onText(/asf/, msg => {
   if (msg.chat.id == admin) {
     bot
       .sendMessage(msg.chat.id, "What do you want?", {
-        parse_mode: "Markdown",
         reply_markup: {
           keyboard: [
             ["status", "help"],
@@ -56,7 +54,7 @@ bot.onText(/asf/, msg => {
           force_reply: true
         }
       })
-      .then(payload => {
+      .then(() => {
         bot.once("message", msg => {
           request(
             {
@@ -70,7 +68,6 @@ bot.onText(/asf/, msg => {
             function(error, response, body) {
               try {
                 bot.sendMessage(msg.chat.id, JSON.parse(body).Result, {
-                  parse_mode: "Markdown",
                   reply_markup: {
                     keyboard: [["asf", "hentai"]]
                   }
@@ -80,7 +77,6 @@ bot.onText(/asf/, msg => {
                   msg.chat.id,
                   "Sorry, something goes wrong\n" + body,
                   {
-                    parse_mode: "Markdown",
                     reply_markup: {
                       keyboard: [["asf", "hentai"]]
                     }
@@ -89,6 +85,59 @@ bot.onText(/asf/, msg => {
               }
             }
           );
+        });
+      });
+  }
+});
+
+bot.onText(/hentai/, msg => {
+  if (msg.chat.id == admin) {
+    bot
+      .sendMessage(msg.chat.id, "Are you a hetai?", {
+        reply_markup: {
+          keyboard: [["prpr"]],
+          force_reply: true
+        }
+      })
+      .then(() => {
+        bot.once("message", msg => {
+          if (msg.text == "prpr") {
+            request(
+              "https://konachan.com/post.json?tags=ass&limit=50",
+              function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  const result = JSON.parse(body) || [];
+                  const index = parseInt(Math.random() * result.length);
+                  bot
+                    .sendPhoto(msg.chat.id, result[index].file_url, {
+                      caption: "手冲一时爽，一直手冲一直爽",
+                      reply_markup: {
+                        keyboard: [["asf", "hentai"]]
+                      }
+                    })
+                    .catch(err => {
+                      bot.sendMessage(msg.chat.id, "手冲失败", {
+                        reply_markup: {
+                          keyboard: [["asf", "hentai"]]
+                        }
+                      });
+                    });
+                } else {
+                  bot.sendMessage(msg.chat.id, "手冲失败", {
+                    reply_markup: {
+                      keyboard: [["asf", "hentai"]]
+                    }
+                  });
+                }
+              }
+            );
+          } else {
+            bot.sendMessage(msg.chat.id, "返回", {
+              reply_markup: {
+                keyboard: [["asf", "hentai"]]
+              }
+            });
+          }
         });
       });
   }
